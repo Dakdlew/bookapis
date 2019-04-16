91068e3eb3fa006cd22003015587b17ec528e513
Dakota <dakdlew@umich.edu>
2019-04-15 23:22:17 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 var config = {
   NYT_KEY : "0WXZYWk1o0oWLYeqyUqfcHFNDvu9ox9K" ,
-  GOOGLE_BOOKS_KEY : "AIzaSyC6wYwXe5ToWe5FXoVnzZtgrS74_Nb1cJ8"
+  GOOGLE_BOOKS_KEY : "AIzaSyClXNNKPJzWv29B9bKOt6NkdLBpM6Irc2I"
 }
